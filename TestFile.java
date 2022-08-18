1aba78db4a014e3057b0588be24ebceb77ebe242
unknown <pransukh.21@gmail.com>
2022-08-18 16:31:24 +0530

    TBuild - 2

---
+++ TestFile.java
@@ -0,0 +1,6 @@
+public class TestFile {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World auto build TestFile ");
+   }
+}
